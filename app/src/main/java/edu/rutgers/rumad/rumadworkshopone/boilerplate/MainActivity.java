62d2f8747fb0d3c779fa6384fb6a11ede684353d
Shreyas <jjman98@yahoo.com>
2015-03-07 15:15:01 -0500

    First

---
+++ app/src/main/java/edu/rutgers/rumad/rumadworkshopone/boilerplate/MainActivity.java
@@ -0,0 +1,45 @@
+package edu.rutgers.rumad.rumadworkshopone.boilerplate;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import edu.rutgers.rumad.rumadworkshopone.R;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_boilerplate);
+        //TODO: 1.Create Layout, 2. Take Input for User, 3. Make it appear in specified color, 4. Go to a new screen
+
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
